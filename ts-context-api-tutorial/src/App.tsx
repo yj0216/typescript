6ae0bdcaa0f6586c08309c3da355c96951236355
yj0216 <yeojun0216@naver.com>
2023-07-11 21:16:31 +0900

    ts todolist study

---
+++ ts-context-api-tutorial/src/App.tsx
@@ -0,0 +1,14 @@
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
+import { TodosContextProvider } from "./contexts/TodosContext"
+
+const App = () => {
+  return (
+    <TodosContextProvider>
+      <TodoForm/>
+      <TodoList/>
+    </TodosContextProvider>
+  )
+}
+
+export default App
